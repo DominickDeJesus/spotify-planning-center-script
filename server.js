60f14f519fe639968e52926ae7bdcf1df37f0718
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2023-05-15 16:32:01 -0400

    added hook endpoint

--- server.js
+++ server.js
@@ -18,39 +18,47 @@ let spotifyToken, spotifyRefreshToken;
 const cron = require("node-schedule");
 const open = require("open");
 const PORT = process.env.PORT || 8888;
-const browserOption = {args: ["--no-sandbox","--disable-setuid-sandbox"], executablePath:"chromium-browser"};
-const {addYouTubeVideos} = require("./api/youtubeapi");
-let page, browser; 
+const browserOption = {
+	args: ["--no-sandbox", "--disable-setuid-sandbox"],
+	executablePath: "chromium-browser",
+};
+const { addYouTubeVideos } = require("./api/youtubeapi");
+let page, browser;
 //await page.click();
 //addYouTubeVideos(["asdfa"]);
-async function launchBrowser(){
-	try{
-	browser = await puppeteer.launch({
-		headless: false,
-		...browserOption
-	});
-	page = await browser.newPage();
-	await page.goto( "http://localhost:8888",{
-		waitUntil: "load",
-	});
-	//await page.setDefaultNavigationTimeout(0);
-	await page.click("#login > a");
-	await page.waitForNavigation();
-	await page.click("button[data-testid='facebook-login']");
-	const email = process.env.EMAIL.toString();
-	const pass = process.env.PASSWORD.toString();
-	console.log("Waiting for page navigation...")
-	await page.waitForSelector("#email");
+async function launchBrowser() {
+	try {
+		browser = await puppeteer.launch({
+			headless: false,
+			...browserOption,
+		});
+		page = await browser.newPage();
+		await page.goto("http://localhost:8888", {
+			waitUntil: "load",
+		});
+		//await page.setDefaultNavigationTimeout(0);
+		await page.click("#login > a");
+		await page.waitForNavigation();
+		await page.click("button[data-testid='facebook-login']");
+		const email = process.env.EMAIL.toString();
+		const pass = process.env.PASSWORD.toString();
+		console.log("Waiting for page navigation...");
+		await page.waitForSelector("#email");
 		//await page.waitForNavigation({waitUntil: 'networkidle0'});
-	console.log("Typing email");
-	await page.evaluate(text => document.getElementById("email").value =text, email);
-	await page.evaluate(text => document.getElementById("pass").value = text, pass );
-	await page.click("#loginbutton");
-	}catch(e){
+		console.log("Typing email");
+		await page.evaluate(
+			(text) => (document.getElementById("email").value = text),
+			email
+		);
+		await page.evaluate(
+			(text) => (document.getElementById("pass").value = text),
+			pass
+		);
+		await page.click("#loginbutton");
+	} catch (e) {
 		console.log(e);
 	}
 }
-
 
 app.use(morgan("dev"));
 app
@@ -153,11 +161,21 @@ app.get("/refresh_token", async function (req, res) {
 	}
 });
 
+app.post("/plohooks", async function (req, res) {
+	try {
+		res.send({
+			response: req,
+		});
+	} catch (error) {
+		res.send(error);
+	}
+});
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-try{
-launchBrowser();
-}catch(e){
+try {
+	launchBrowser();
+} catch (e) {
 	console.log(e);
 }
 //open(process.env.HOME_URL);
